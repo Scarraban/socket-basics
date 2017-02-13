7aeaae7957678436dc763d69500ad371bbad8de9
Sam Butler-Thompson <sam@q2q-it.com>
2017-02-13 19:33:56 +0000

    Added customer message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to server');
 });
+
+socket.on('message', function(message) {
+	console.log('New mesage:');
+	console.log(message.text);
+});
