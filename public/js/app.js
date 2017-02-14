8e1522fe62f5a500a1427f916dc8dffb02672460
Sam Butler-Thompson <sam@q2q-it.com>
2017-02-14 15:23:12 +0000

    Added form to submit messages via Socket.io

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,20 @@ socket.on('connect', function() {
 });
 
 socket.on('message', function(message) {
-	console.log('New mesage:');
+	console.log('New message:');
 	console.log(message.text);
+});
+
+// On sumbit of message
+var $form = jQuery('#message-form');
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+  $message = $form.find('input[name=message]');
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+  
+  $message.val('').focus();
 });
